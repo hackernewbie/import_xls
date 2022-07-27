31293ec8805aeed26f69874fe2a48bc5bcc323fb
Rajiv Verma <rajiv@webguy.in>
2022-07-27 22:34:23 +0530

    Some fizes

--- app/Http/Controllers/ExcelDataController.php
+++ app/Http/Controllers/ExcelDataController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Importer;
 use App\Models\ExcelData;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Importer;
 
 
 class ExcelDataController extends Controller
